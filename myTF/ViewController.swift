1cd7e7a9828647c8c45ba7e511d4677421e604b8
D7703_13 <d7703_13@D7703-13ui-iMac.local>
2018-03-19 15:59:50 +0900

    123

--- myTF/ViewController.swift
+++ myTF/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var myLB: UILabel!
+    @IBOutlet weak var myTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTF.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func myBT(_ sender: UIButton) {
+        myLB.text = myTF.text
+        myTF.text = ""
+        myTF.resignFirstResponder()
+        
+    }
+    //UITFdelegate method
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // called when 'return' key pressed. return NO to ignore.
+        myTF.resignFirstResponder()
+          return true
+    }
 }
-
